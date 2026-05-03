55302c28cca3386842638da9fbb8ecf108da1c53
caloneco66-tech <caloneco66@gmail.com>
2026-05-03 22:30:32 +0000

    Add files via upload

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ const PRODUCTS = [
     genre: "Pour homme et femme",
     volume: "125 ml",
     nouveaute: true,
-    images: ["img/sakura.jpg"],
+    images: ["IMG/sakura.jpg"],
     emoji: "&#127800;"
   },
   {
